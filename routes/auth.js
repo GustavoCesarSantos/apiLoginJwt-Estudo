eb3548644b80e0f8cdcc60dd81bd6da3cdf3a38c
GustavoCesarSantos <gustavocs789@gmail.com>
2019-11-06 21:43:32 -0300

    @hapi/joi instalado e schema para validacao e validacao do req.body criados e funcionando.

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
+const Joi = require('@hapi/joi');
 
 const Users = require('../model/Users');
 
+const userSchemaValidate = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(8).max(1024).required()
+})
+
 router.post('/register', async (req,res) => {
-  try{
+  //Validate the data before we create user
+  const { error } = userSchemaValidate.validate(req.body);
+  if(error){
+    return res.status(400).send(error.details[0].message)
+  }
+
+  try{  
     const user = new Users({
       name: req.body.name,
       email: req.body.email,
@@ -12,7 +25,7 @@ router.post('/register', async (req,res) => {
     const savedUser = await user.save()
     res.status(200).send(savedUser);
   }catch(error){
-    res.status(400).send('Error: ',error);
+    res.status(400).send(error);
   }
 });
 
