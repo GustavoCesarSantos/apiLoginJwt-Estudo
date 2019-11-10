fc301465ffd3f5101f04bbfd39910998b5386f7b
GustavoCesarSantos <gustavocs789@gmail.com>
2019-11-10 16:35:48 -0300

    Adicionado pacote cors para conseguir usar back e front de maneira plena

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 dotenv.config()
