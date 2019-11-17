7ba382b215baa8d0946e3a05375afca50c20d330
GustavoCesarSantos <gustavocs789@gmail.com>
2019-11-16 23:58:15 -0300

    Ajustando Bearer em auth e verifyToken

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function auth(req, res, next){
   try{
-    const token = req.header('auth-token');
+    const token = req.header('Bearer');
     if(!token)
       return res.status(401).send('Access Denied');
     
--- routes/auth.js
+++ routes/auth.js
@@ -53,7 +53,7 @@ router.post('/login', async (req,res) => {
     
     //Create and asign token
     const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-    res.header('auth-token', token).send(token);
+    res.header('Bearer', token).send(token);
     
     // res.status(200).send('Logged in');
   }catch(error){
